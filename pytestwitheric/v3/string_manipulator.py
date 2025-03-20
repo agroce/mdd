df57a8602f638117478b75fcc6af6eb8f22fd391
agroce <agroce@gmail.com>
2025-03-20 10:46:22 -0700

    v3 fix

--- pytestwitheric/v3/string_manipulator.py
+++ pytestwitheric/v3/string_manipulator.py
@@ -1,5 +1,5 @@
 class StringManipulator:  
-    def to_lower_case(self, my_string:  
-        if not isinstance(my_string, str):  
-            return "Invalid input"  
+    def to_lower_case(self, my_string):
+        if not isinstance(my_string, str):
+            return "Invalid input"
         return my_string.lower()
